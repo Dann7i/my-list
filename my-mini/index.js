ac0633712f3191befa29972ec314b7b011e78b2d
Dann7i <hermawanwildan78@gmail.com>
2025-08-22 06:43:04 +0700

    data

--- my-mini/index.js
+++ my-mini/index.js
@@ -1,2 +1,10 @@
 import fs from "fs";
 const FILE = "data.json";
+
+function loadTask() {
+  if (!fs.existsSync(FILE)) return [];
+  const data = fs.readFileSync(FILE);
+  return JSON.parse(data);
+}
+const lo = loadTask();
+console.log(lo);
