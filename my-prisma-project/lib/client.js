0ef896e00c5901b20360226e51255e3bd8152329
Dann7i <hermawanwildan78@gmail.com>
2025-08-26 19:32:35 +0700

    initial commit

--- my-prisma-project/lib/client.js
+++ my-prisma-project/lib/client.js
@@ -1,3 +1,8 @@
 import { prismaClient } from "@prisma/client";
 
 const prisma = globalThis.prisma || new prismaClient();
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
+
+export default prisma;
