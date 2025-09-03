57f671b0691c80cd3293577a0a06d592c9f7208e
Dann7i <hermawanwildan78@gmail.com>
2025-09-03 18:12:42 +0700

    commit

--- bot-tele/storage/index.js
+++ bot-tele/storage/index.js
@@ -1,1 +1,6 @@
-console.log("testing dimulai..");
+import TelegramBot from "node-telegram-bot-api";
+
+const token = "8099406581:AAH2fEMnidQcfulmXiHbclaOsNf-HdCkI0k";
+
+const bot = new TelegramBot(token, { polling: true });
+console.log(bot);
