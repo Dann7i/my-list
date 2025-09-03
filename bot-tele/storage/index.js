851cadaf41a78afd6fee2ffaa572540955f60c8c
Dann7i <hermawanwildan78@gmail.com>
2025-09-03 19:05:42 +0700

    index.js

--- bot-tele/storage/index.js
+++ bot-tele/storage/index.js
@@ -1,11 +1,14 @@
 import TelegramBot from "node-telegram-bot-api";
 console.log("Bot started...");
 const token = "8099406581:AAH2fEMnidQcfulmXiHbclaOsNf-HdCkI0k";
-
+let started = /Halo/;
+let chatId;
 const danbot = new TelegramBot(token, { polling: true });
 
 danbot.on("message", (pesan) => {
-  const chatId = pesan.chat.id;
-  danbot.sendMessage(chatId, "Hello, I'm your bot!");
+  pesan.chat.id;
   console.log("Pesan diterima:", pesan.text);
+});
+danbot.onText(started, (pesan) => {
+  danbot.sendMessage(pesan.chat.id, "Halo! Ada yang bisa saya bantu?");
 });
