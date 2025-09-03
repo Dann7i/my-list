7cf6b113ed0b08e4ac1e94b74bc10f4ff375d570
Dann7i <hermawanwildan78@gmail.com>
2025-09-03 18:39:47 +0700

    index.js

--- bot-tele/storage/index.js
+++ bot-tele/storage/index.js
@@ -1,6 +1,5 @@
 import TelegramBot from "node-telegram-bot-api";
-
+console.log("Bot started...");
 const token = "8099406581:AAH2fEMnidQcfulmXiHbclaOsNf-HdCkI0k";
 
 const bot = new TelegramBot(token, { polling: true });
-console.log(bot);
